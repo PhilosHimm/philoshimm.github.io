37b5e92f129dbad3c56bc67590f465cba350d0e8
PhilosHimm <161180159+PhilosHimm@users.noreply.github.com>
2024-11-03 20:37:49 -0500

    updates

--- Final Project/project.js
+++ Final Project/project.js
@@ -1,4 +1,6 @@
+
 // Function to scroll to the Work Experience section
+
 function scrollToWorkExperience() {
     const workExperienceSection = document.getElementById("work-experience");
     workExperienceSection.scrollIntoView({ behavior: "smooth" });
