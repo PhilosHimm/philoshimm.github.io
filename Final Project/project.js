2eff0fb22910f99cd5163c36ead93f371cf7f9bf
PhilosHimm <161180159+PhilosHimm@users.noreply.github.com>
2024-11-03 22:36:23 -0500

    updates

--- Final Project/project.js
+++ Final Project/project.js
@@ -5,4 +5,11 @@ function scrollToWorkExperience() {
     const workExperienceSection = document.getElementById("work-experience");
     workExperienceSection.scrollIntoView({ behavior: "smooth" });
   }
+
+// Function to scroll to the contact section
+function scrollToContact() {
+    const workExperienceSection = document.getElementById("contactMe");
+    workExperienceSection.scrollIntoView({ behavior: "smooth" });
+  }
+  
   
